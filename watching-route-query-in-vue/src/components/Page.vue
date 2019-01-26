b1bab8f87061e81a7a7eaf4adde219a28b5ef50f
KimHyeshin <hyeshin2530@gmail.com>
2019-01-27 00:10:10 +0900

    watch 추가

--- watching-route-query-in-vue/src/components/Page.vue
+++ watching-route-query-in-vue/src/components/Page.vue
@@ -1,10 +1,38 @@
 <template>
-    <div>Page</div>
+    <div>
+      <div>Page</div>
+      <div>
+        <ul>
+          <li>name : {{name}}</li>
+          <li>color : {{color}}</li>
+        </ul>
+      </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Page"
+        name: "Page",
+      data: () => {
+          return {
+            name : '',
+            color : ''
+          }
+      },
+      created(){
+          console.log("this.$route.query");
+          console.log(this.$route.query);
+          this.name = this.$route.query.name;
+          this.color = this.$route.query.color;
+      },
+      watch : {
+          '$route.query'() {
+            console.log('watch!!!')
+            this.name = this.$route.query.name;
+            this.color = this.$route.query.color;
+          }
+      }
+
     }
 </script>
 
