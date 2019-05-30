bd3c96a9c69c1d266d5fa1a93482d19918a07a0b
김혜신 <hyeshin2530@gmail.com>
2019-05-30 16:49:24 +0900

    디렉토리 구분하여 파일 저장

--- file-upload-module/server/api/upload/index.js
+++ file-upload-module/server/api/upload/index.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 const multer = require('multer');
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './data/')
+        // key 확인
+        console.log(req.params.key);
+
+        // type 확인
+        let type = file.fieldname.split('_')[0]
+
+        // 폴더 생성
+        fs.mkdir('./data/' + type + '/' + req.params.key + '/', function(err){
+            // if ( err.code != 'EEXIST' ) throw err;
+            // console.log('err :: ' + err.code)
+
+            cb(null, './data/' + type + '/' + req.params.key + '/')
+        });
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + '.png')
+        cb(null, file.fieldname + '.png')
     }
 })
 
 const upload = multer({ storage : storage });
 
-var cpUpload = upload.fields([{ name: 'image01', maxCount: 1 }, { name: 'image02', maxCount: 1 }])
+var cpUpload = upload.fields([{ name: 'image_01', maxCount: 1 }, { name: 'image_02', maxCount: 1 },{ name: 'thumbnail_01', maxCount: 1 }, { name: 'thumbnail_02', maxCount: 1 }])
 
-router.post('/image', cpUpload, function (req, res, next) {
+router.post('/image/:key', cpUpload, function (req, res, next) {
     console.log("image upload success")
     res.json({ status: 200, msg: "이미지 업로드 완료" })
 })
